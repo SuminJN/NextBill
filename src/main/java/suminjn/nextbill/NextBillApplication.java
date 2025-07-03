9143ca1c4563aa36766b6bf74cbc6d8743faa343
Sumin Jeon <wjstnals1211@gmail.com>
2025-07-03 23:55:04 +0900

    initialize Spring Boot project

---
+++ src/main/java/suminjn/nextbill/NextBillApplication.java
@@ -0,0 +1,13 @@
+package suminjn.nextbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextBillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextBillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/suminjn/nextbill/NextBillApplicationTests.java
@@ -0,0 +1,13 @@
+package suminjn.nextbill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextBillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
