415b4fb0c589610abdfeba1f1a636151f42fbc07
Sumin Jeon <wjstnals1211@gmail.com>
2025-07-05 14:08:54 +0900

    feat: OpenAPI 구성 추가 및 API 문서화 설정

---
+++ src/main/java/suminjn/nextbill/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package suminjn.nextbill.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("NextBill API")
+                        .description("구독 서비스 관리 및 알림 API")
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
