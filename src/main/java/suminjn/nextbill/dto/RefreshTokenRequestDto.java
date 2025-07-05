18fba040e9a03a1cc1ceca9af91e2444690700d4
Sumin Jeon <wjstnals1211@gmail.com>
2025-07-05 14:09:03 +0900

    feat: RefreshTokenRequestDto에 @NotBlank 어노테이션 추가

--- src/main/java/suminjn/nextbill/dto/RefreshTokenRequestDto.java
+++ src/main/java/suminjn/nextbill/dto/RefreshTokenRequestDto.java
@@ -1,9 +1,12 @@
 package suminjn.nextbill.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RefreshTokenRequestDto {
     private String userEmail;
+    
+    @NotBlank(message = "리프레시 토큰은 필수입니다")
     private String refreshToken;
 }
